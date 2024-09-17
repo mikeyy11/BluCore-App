c45b50b0b7f073975c2a823146b11905f9b65d88
Pirashanthan <sivakumaran.pirashanthan@directfn.com>
2024-09-17 19:18:26 +0530

    added services tabs

---
+++ app/src/main/java/com/example/blucore/DataNoteImformation.java
@@ -0,0 +1,7 @@
+package com.example.blucore;
+
+public class DataNoteImformation {
+    public static String[] textArray = {"Lightshot 截圖工具", "Lightshot 截圖工具"};
+    public static String[] dateArray = {"2017-04-25", "2024-09-15"};
+    public static String[] id = {"1","2"};
+}
